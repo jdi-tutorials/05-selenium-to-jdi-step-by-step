1446a0e541df88494240d6cf67a9a1450f2215a1
Roman_Iovlev <roman_iovlev@epam.com>
2019-10-26 01:01:46 +0300

    Step 13 - Optimize DataClass

--- src/main/java/jdisite/entities/DefaultData.java
+++ src/main/java/jdisite/entities/DefaultData.java
@@ -1,5 +1,7 @@
 package jdisite.entities;
 
 public class DefaultData {
-    public static User ROMAN = new User("Roman", "Jdi1234");
+    public static User ROMAN = new User().set(u-> {
+        u.name = "Roman"; u.password = "Jdi1234";
+    });
 }
--- src/test/java/com/jdi/test/data/DefaultDataProvider.java
+++ src/test/java/com/jdi/test/data/DefaultDataProvider.java
@@ -3,14 +3,17 @@ package com.jdi.test.data;
 import jdisite.entities.ContactInfo;
 
 public class DefaultDataProvider {
-    public static ContactInfo SIMPLE_CONTACT = new ContactInfo()
-        .setName("Roman").setLastName("Iovlev").setPosition("ChiefQA")
-        .setPassportNumber(4321).setPassportSeria(123456)
-        .setDescription("JDI - awesome UI automation tool");
+    public static ContactInfo SIMPLE_CONTACT = new ContactInfo().set(c-> {
+        c.name = "Roman"; c.lastName = "Iovlev"; c.position = "ChiefQA";
+        c.passportNumber = 4321; c.passportSeria = 123456;
+        c.description = "JDI - awesome UI automation tool";
+    });
 
-    public static ContactInfo FULL_CONTACT = new ContactInfo(
-        "Roman", "Full Contact", "ChiefQA","Female", "Other",
-        "Sun, Snow","JDI - awesome UI automation tool",
-        4321, 123456, true, false
-    );
+    public static ContactInfo FULL_CONTACT = new ContactInfo().set(c-> {
+        c.name = "Roman"; c.lastName = "Full Contact"; c.position = "ChiefQA";
+        c.gender = "Female"; c.religion = "Other"; c.weather = "Sun, Snow";
+        c.passportNumber = 4321; c.passportSeria = 123456;
+        c.description = "JDI - awesome UI automation tool";
+        c.passport = true; c.acceptConditions = false;
+    });
 }
