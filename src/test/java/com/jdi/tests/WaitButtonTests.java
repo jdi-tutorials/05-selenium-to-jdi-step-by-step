fa59dd4fe7782137e1ac42ad6e6064a2e80e47c4
Roman_Iovlev <roman_iovlev@epam.com>
2019-10-26 00:08:21 +0300

    Step 7 - remove unnecessary waits and Thread Sleeps

--- src/test/java/com/jdi/tests/WaitButtonTests.java
+++ src/test/java/com/jdi/tests/WaitButtonTests.java
@@ -1,18 +1,15 @@
 package com.jdi.tests;
 
 import com.jdi.TestsInit;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import static com.epam.jdi.light.driver.WebDriverFactory.getDriver;
 import static com.jdi.states.State.loggedIn;
-import static java.time.Duration.ofSeconds;
 import static jdisite.enums.MenuOptions.ElementsPacks;
 import static jdisite.enums.MenuOptions.HTML5;
 import static jdisite.pages.JDISite.htmlPage;
 import static jdisite.pages.JDISite.selectInMenu;
-import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOf;
 import static org.testng.Assert.assertEquals;
 
 public class WaitButtonTests implements TestsInit {
@@ -25,10 +22,6 @@ public class WaitButtonTests implements TestsInit {
     @Test
     public void waitButtonTest() {
         htmlPage.checkOpened();
-        //Thread.sleep(3000);
-        WebDriverWait wait = new WebDriverWait(getDriver(), ofSeconds(4));
-        wait.until(visibilityOf(htmlPage.suspendButton));
-
         htmlPage.suspendButton.click();
         assertEquals(getDriver().switchTo().alert().getText(), "Suspend button");
     }
