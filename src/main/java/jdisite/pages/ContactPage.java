ca08e52d7e3ce5e9e9518e5a6a3bf39a9bb83f35
Roman_Iovlev <roman_iovlev@epam.com>
2019-10-26 00:51:48 +0300

    Step 12 - Short Contact form

--- src/main/java/jdisite/pages/ContactPage.java
+++ src/main/java/jdisite/pages/ContactPage.java
@@ -1,13 +1,15 @@
 package jdisite.pages;
 
+import com.epam.jdi.light.elements.composite.Form;
 import com.epam.jdi.light.elements.composite.WebPage;
 import com.epam.jdi.light.elements.pageobjects.annotations.Title;
 import com.epam.jdi.light.elements.pageobjects.annotations.Url;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.UI;
+import jdisite.entities.ContactInfo;
 import jdisite.sections.ContactForm;
-import jdisite.sections.ShortContactForm;
 
 @Url("/contacts.html") @Title("Contact Form")
 public class ContactPage extends WebPage {
     public static ContactForm contactForm;
-    public static ShortContactForm sContactForm;
+    @UI("#contact-form") public static Form<ContactInfo> sContactForm;
 }
